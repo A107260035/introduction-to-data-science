46d5bb7a696cd5a6858c33ed7f4cf7908df765e2
minhanl <61857174+minhanl@users.noreply.github.com>
2020-04-24 23:26:16 +0800

    Created using Colaboratory

---
+++ week3_A107260035_李旻翰.py
@@ -0,0 +1,68 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "week3_A107260035_李旻翰.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyNs9RDNPbLoJhITZBifmkR6",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/minhanl/introduction-to-data-science/blob/master/week3_A107260035_%E6%9D%8E%E6%97%BB%E7%BF%B0.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "e3-e7a6Hik_g",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 118
+        },
+        "outputId": "6a787ce9-2360-42de-cca5-5733f7002d86"
+      },
+      "source": [
+        "movie_time = 181\n",
+        "movie_rating = 8.5\n",
+        "movie_title = \"Avengers: Endgame\"\n",
+        "\n",
+        "print(movie_time) \n",
+        "print(movie_rating) \n",
+        "print(movie_title)\n",
+        "\n",
+        "print(type(movie_time)) \n",
+        "print(type(movie_rating)) \n",
+        "print(type(movie_title))"
+      ],
+      "execution_count": 4,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "181\n",
+            "8.5\n",
+            "Avengers: Endgame\n",
+            "<class 'int'>\n",
+            "<class 'float'>\n",
+            "<class 'str'>\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
